a52e7ef882ca0cd76cb0d01299504d9de342e126
Peter Barclay <peter@peter-barclay.com>
2019-11-09 08:07:35 +0000

    Add activity list with 'Add Activity' button

--- HabitTracker/ContentView.swift
+++ HabitTracker/ContentView.swift
@@ -4,14 +4,51 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct AddActivityView: View {
+    var activities: [Activity]
+    
     var body: some View {
-        Text("Hello World")
+        Text("TODO: Add Activity")
+    }
+}
+
+struct ContentView: View {
+    let user: User
+    @State private var showingAddActivity = false
+
+    var body: some View {
+        NavigationView {
+            List {
+                ForEach(user.activities) {
+                    Text("\($0.title)")
+                }
+            }
+            .navigationBarTitle("Activities")
+            .navigationBarItems(trailing:
+                Button(action: {
+                    self.showingAddActivity = true
+                }) {
+                    Image(systemName: "plus")
+                }
+            )
+            .sheet(isPresented: $showingAddActivity) {
+                AddActivityView(activities: self.user.activities)
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
+    static let user = User(activities: [
+        Activity(title: "Acitivty 1",
+                 description: "Activity 1 Detail"),
+        Activity(title: "Acitivty 2",
+                 description: "Activity 2 Detail"),
+        Activity(title: "Acitivty 2",
+                 description: "Activity 2 Detail"),
+    ])
+
     static var previews: some View {
-        ContentView()
+        ContentView(user: user)
     }
 }
--- HabitTracker/User.swift
+++ HabitTracker/User.swift
@@ -5,5 +5,9 @@
 import Foundation
 
 class User: ObservableObject {
-    @Published var activities = [Activity]()
+    @Published var activities: [Activity]
+    
+    init(activities: [Activity] = []) {
+        self.activities = activities
+    }
 }
